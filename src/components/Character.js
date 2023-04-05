671d69bda25f822dc0b92f05463395a2ac8203db
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2023-04-05 12:34:57 -0600

    Built the basic beginning of Character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,11 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const Character = (props) => {
+    return (
+        <p>{props.name}</p>
+    )
+}
+
+export default Character;
